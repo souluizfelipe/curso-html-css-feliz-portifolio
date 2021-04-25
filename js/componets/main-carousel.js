46f0f7f7ded298dd88cbee33e5c9f5598baf71b1
Luiz Felipe Pereira <30962248+souluizfelipe@users.noreply.github.com>
2021-04-25 10:44:29 -0300

    create animation for carousel

--- js/componets/main-carousel.js
+++ js/componets/main-carousel.js
@@ -2,15 +2,15 @@ const btnRight = document.querySelector('.button-arrow.-right');
 const btnLeft = document.querySelector('.button-arrow.-left');
 const elements = document.querySelector('.elements');
 
-
-let pos = 100;
+let pos = 0;
 
 btnRight.addEventListener('click', function() {
-    pos = pos + 100;
+    pos = pos + 150;
     elements.style = `transform: translateX(${pos}px)`;
 });
 
 btnLeft.addEventListener('click', function() {
-    pos = pos - 100;
+    pos = pos - 150;
     elements.style = `transform: translateX(${pos}px)`;
 });
+
